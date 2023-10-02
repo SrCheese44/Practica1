96b71cd43989a4188d27ab5c4d12e26d6d8f6ad3
SrCheese44 <sr.cheese44@gmail.com>
2023-10-02 19:26:12 +0100

    Cambio de color
    Cambio de color al entrar en contacto con entidades

--- Assets/Scripts/Movimientos.cs
+++ Assets/Scripts/Movimientos.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Movimientos : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer rbSprite;
     //variables asociadas al movimiento
     public float speed;
     private float move;
 
-    
+    private bool stun;
+    private float time;
+
   
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rbSprite = GetComponent<SpriteRenderer>(); 
+        
+       
     }
     void Update()
     {
@@ -25,20 +33,40 @@ public class Movimientos : MonoBehaviour
         move = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(rb.velocity.x, move * speed);
 
+        if (stun == true)
+        {
+            time = Time.deltaTime + time;
+            rb.Sleep();
+            rbSprite.color = UnityEngine.Color.red;
+        }
+        if (stun == true && time > 2)
+        {
+            rb.WakeUp();
+            stun = false;         
+            time = 0;
+            rbSprite.color = new UnityEngine.Color(0.4589239f, 0.2766109f, 0.5283019f, 1f);
+
+        }
+
 
 
     }
-   /* private void OnCollisionEnter2D(Collision2D collision)
+  
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bordes"))
         {
-
-            Time.timeScale = 0;
-
+          
+            stun = true;
+           
         }
-       
-    */
-    }
+
+      
+
+
+    }   
+  
+ }
 
 
 
